3d42dfa59d3581f27afa8f0957ee4a7ae66c5b3d
namanshukla8707 <113860823+namanshukla8707@users.noreply.github.com>
2023-07-11 14:10:03 +0530

    iNotebook

---
+++ backend/db.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook";
+const connectToMongo = () => {
+  mongoose.connect(mongoURI);
+  console.log("connected to mongo sucessfully");
+};
+module.exports = connectToMongo;
